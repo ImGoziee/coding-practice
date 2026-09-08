85867c62aae03981e02cd784681bbdbc19846084
ImGoziee <ghozimalang87@gmail.com>
2026-09-08 13:55:09 +0700

    file belajar variabel

---
+++ jobsheet2/ContohVariabel10.java
@@ -0,0 +1,14 @@
+public class ContohVariabel10 {
+    public static void main(String[] args) {
+        String hobiSaya = "Bermain petak umpet";
+        boolean isPandai = true;
+        char jenisKelamin = 'L';
+        byte umurSaya = 18;
+        double ipk = 3.24, tinggiBadan = 1.78;
+        System.out.println(hobiSaya);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umur saya sekarang: " + umurSaya);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", + ipk, tinggiBadan));
+    }
+}
