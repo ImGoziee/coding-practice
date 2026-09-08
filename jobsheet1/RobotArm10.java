39c3ef062caa08eeb3b73dd383988e148b8fc7ff
ImGoziee <ghozimalang87@gmail.com>
2026-09-08 12:48:18 +0700

    Initial commit

---
+++ jobsheet1/RobotArm10.java
@@ -0,0 +1,8 @@
+public class RobotArm10 {
+    public static void main(String[] args) {
+        System.out.println("Setelah langkah 1: Nampan A = Kosong, Nampan B = 1 Bola Bulan, Nampan C = 1 Bola Bintang");
+        System.out.println("Setelah langkah 2: Nampan A = 1 Bola Bulan, Nampan B = Kosong, Nampan C = 1 Bola Bintang");
+        System.out.println("Setelah langkah 3: Nampan A = 1 Bola Bulan, Nampan B = 1 Bola Bintang, Nampan C = Kosong");
+        System.out.println("Kesimpulan: Pernyataan yang benar adalah A dan E");
+    }
+}
