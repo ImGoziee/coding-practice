58a3c533c45d4d4ec41e72f621dac74e8bc5f445
ImGoziee <ghozimalang87@gmail.com>
2026-09-12 13:33:30 +0700

    update

--- jobsheet3/MenghitungTotalBayar10.java
+++ jobsheet3/MenghitungTotalBayar10.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class MenghitungTotalBayar10 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int harga;
+        double diskon = 0.15, potongan, jmlBayar;
+
+        System.out.print("Masukkan harga barang : ");
+        harga = sc.nextInt();
+
+        potongan = harga * diskon;
+        jmlBayar = harga - potongan;
+
+        System.out.println("Diskon : " + potongan);
+        System.out.println("Jumlah bayar : " + jmlBayar);
+    }
 }
