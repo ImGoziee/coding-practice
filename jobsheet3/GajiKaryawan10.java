c610118f176b3308da0410db327a285f3b8794d3
ImGoziee <ghozimalang87@gmail.com>
2026-09-21 14:43:59 +0700

    update

--- jobsheet3/GajiKaryawan10.java
+++ jobsheet3/GajiKaryawan10.java
@@ -9,14 +9,14 @@ public class GajiKaryawan10 {
         double 
         tunjanganTransport = 600000,
         tunjanganMakanan = 400000,
-        bonus, 
-        totalGaji;
+        bonus; 
+        int totalGaji;
 
         System.out.print("Masukkan gaji pokok : ");
         gajiPokok = sc.nextInt();
 
         bonus = 0.05 * gajiPokok;
-        totalGaji = gajiPokok + tunjanganTransport + tunjanganMakanan + bonus - (0.1 * gajiPokok);
+        totalGaji = (int) (gajiPokok + tunjanganTransport + tunjanganMakanan + bonus - (0.1 * gajiPokok));
 
         System.out.println("Bonus bulanan anda adalah : " + bonus);
         System.out.println("Total gaji yang diterima adalah : " + totalGaji);
